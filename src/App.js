54847cc5fcf913ca4fb9a9169528362ee2bb8b79
ostapfylypets <ostapfull21@gmail.com>
2021-05-11 18:44:02 +0300

    hw1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
+import Car from "./component/Car";
 import './App.css';
 
 function App() {
   return (
     <div >
-
+      <Car model={'mers'} description={'1'} power={300+' ,'} volume={10}/>
+      <Car  model={'bmw'} description={'2'} power={310+' ,'} volume={10}/>
+      <Car  model={'toyota'} description={'3'} power={320+' ,'} volume={3.5}/>
     </div>
   );
 }
